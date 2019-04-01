53489aa25966e9bd010b33dff184c498e38ed024
jugonzal <jugonzal@gmail.com>
2019-04-01 16:05:02 -0400

    Check password length

--- passcheck.js
+++ passcheck.js
@@ -1,12 +1,19 @@
 // 1. We'll pass a password in the command line
 
-var password = process.argv 
+var password = process.argv[2] 
 
-console.log("Is this the password? ", password[2])
+console.log("Is this the password? ", password)
 
 // 2. We'll check many of these rules
 
 // - at least 8 characters
+
+if (password.length >= 8) {
+	console.log("Length is sufficient.")
+} else {
+	console.log("Not long enough.")
+}
+
 // - at least one of them should be a special character (!@#$)
 // - at least one number
 // - at least one upper case
